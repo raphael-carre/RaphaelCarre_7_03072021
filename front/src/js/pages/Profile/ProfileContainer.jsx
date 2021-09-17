3fd6039591684abe3e85c2cda2742c0c08823f21
Raphaël Carré <raphaelcarre@me.com>
2021-09-17 15:08:22 +0200

    Frontend ProfileContainer refactoring

--- front/src/js/pages/Profile/ProfileContainer.jsx
+++ front/src/js/pages/Profile/ProfileContainer.jsx
@@ -1,44 +1,19 @@
 import { LoaderContext } from '@js/utils/context'
-import { useModal } from '@js/utils/hooks'
-import { useFetch } from '@js/utils/hooks'
-import Modal from '@js/utils/Modal'
-import React, { useState, useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
 import ProfileView from './ProfileView'
 
 const ProfileContainer = () => {
-    const { id } = useParams()
+    const id = localStorage.getItem('userId')
     const uri = `/posts/user/${id}`
-
-    const { isLoading, data, error } = useFetch(`/users/${id}`)
-
-    const [userData, setUserData] = useState(null)
-
-    const modal = useModal()
-
-    useEffect(() => {
-        if (error && !error.key) {
-            modal.error(error.statusCode && error.statusCode !== 500 ? error.message : 'Il y a eu un problème')
-        }
-    }, [error])
+    const userData = JSON.parse(localStorage.getItem('userData'))
 
     const {setShowLoader} = useContext(LoaderContext)
 
     useEffect(() => {
-        setShowLoader(isLoading)
-    }, [isLoading])
+        setShowLoader(true)
+    }, [])
 
-    useEffect(() => {
-        data && setUserData(data)
-        data && (document.title = `Groupomania - ${data.firstName} ${data.lastName}`)
-    }, [data])
-
-    return (
-        <>
-            {modal.content && <Modal content={modal.content} type={modal.type} />}
-            {userData && <ProfileView uri={uri} userData={userData} />}
-        </>
-    )
+    return <ProfileView uri={uri} userData={userData} />
 }
 
 export default ProfileContainer
